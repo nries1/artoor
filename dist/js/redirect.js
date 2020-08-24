a43c97689f7f7423b047ddb48781b2af858ea355
Nicolas Ries <nries1@gmail.com>
2020-08-24 16:40:08 -0400

    trigger redirect on load

--- dist/js/redirect.js
+++ dist/js/redirect.js
@@ -1,3 +1,3 @@
-window.addEventListener('loadstart', function() {
+window.addEventListener('load', function() {
   if (window.location.protocol !== 'https:' && !/localhost/.test(window.location.href)) window.location.replace('https://artoor.herokuapp.com');
 });
